4615b7b6a283f6d5de82ff5b04a9b7d7e5a50892
megu-tixya <megumi_tixyatixya@yahoo.co.jp>
2022-02-16 08:32:48 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/ogata-training/s3-bucket/aws"
-  version = "2.14.1"
+  version = "2.2.0"
   # insert required variables here
   bucket_prefix = "ogata-megumi"
 }
